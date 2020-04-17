48b46ebf2f1203e7e5d93bd3fc464599536ec9c4
fibll <acc.simonretsch@icloud.com>
2020-04-17 12:12:26 +0200

    Add testing line to main.c file

--- main.c
+++ main.c
@@ -5,5 +5,6 @@ int main (int argc, char* argv[])
     //Variabeln
     printf("foobar\n");
     printf("Another line for the test\n");
+    printf("Text line added in another branch")
     return 0;
 }
